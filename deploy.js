6bb9ff57176703d2613add4006f37fb6a4d37fa0
Bernardo Loureiro <lourb896@newschool.edu>
2018-11-12 11:23:41 -0200

    Updates

--- deploy.js
+++
@@ -1,6 +0,0 @@
-const ghpages = require('gh-pages');
-
-console.log('--->deploying app...');
-ghpages.publish('dist', function(err) {
-  console.log('--->done.');
-});
--- nwb.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  type: 'web-app',
-  webpack: {
-    // relative paths
-    publicPath: ''
-  }
-}
